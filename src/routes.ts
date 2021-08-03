b88ac0208b9d64dd813d995089b5a9662590c1bb
mcs010 <math.cds10@gmail.com>
2021-08-03 18:46:44 -0300

    feat/tags_middlewares_exception-handling

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from './controllers/CreateTagController';
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
-const router = Router();
+const router = Router()
 
-const createUserController = new CreateUserController(); // Already has the request and the response
+const createUserController = new CreateUserController() // Already has the request and the response
+const createTagController = new CreateTagController()
 
 router.post("/users", createUserController.handle) // createUserController.handle == (request, response) =>{}
+router.post("/tags", ensureAdmin, createTagController.handle)
 
 export { router }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from 'express';
+import express, { Request, Response, NextFunction} from 'express';
+import "express-async-errors";
 
 import { router } from "./routes";
 
@@ -11,6 +12,16 @@ app.use(express.json()) // Habilitates express to work with json
 
 app.use(router); // Inserts the routes into express - like a middleware
 
-app.use((err, request, response, next) => {})
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
 
 app.listen(3000, () => console.log("Server is running"));
